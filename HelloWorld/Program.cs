1141803aad5f0a26797558141c6556cfc6ae02ac
Joe Symons <joe.j.symons@gmail.com>
2024-05-28 17:22:07 +0100

    Completed String Types, Next Booleans

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -68,9 +68,28 @@ namespace HelloWorld
             Console.WriteLine(name);
             Console.WriteLine(letter);
 
-            String ageString = "28"; 
-            int convertedAge = Convert.ToInt32(ageString);
-            Console.WriteLine(convertedAge);
+            string numberString = "24"; 
+            int convertedInt = Convert.ToInt32(numberString);
+            Console.WriteLine(convertedInt);
+
+            string longString = "50000000000";
+            long convertedLong = Convert.ToInt64(longString);
+
+            string floatString = "0.50000";
+            float convertedFloat = Convert.ToSingle(floatString);
+
+            string doubleString = "1.1415973";
+            double convertedDouble = Convert.ToDouble(doubleString);
+
+            string decimalString = "14.33";
+            decimal convertedDecimal = Convert.ToDecimal(decimalString);
+
+
+        }
+
+        static void BooleanType()
+        {
+
         }
 
 
