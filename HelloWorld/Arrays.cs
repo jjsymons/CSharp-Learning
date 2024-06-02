b1fde8a5bfc366d97ef54369c00f238a03c1850b
Joe Symons <joe.j.symons@gmail.com>
2024-06-02 18:21:48 +0100

    Continued work on Arrays

--- HelloWorld/Arrays.cs
+++ HelloWorld/Arrays.cs
@@ -6,7 +6,7 @@ namespace Arrays
 	{
 		public static void Arrays()
 		{
-			int num1 = 10, 
+			/*int num1 = 10, 
 				num2 = 20, 
 				num3 = 40;
 			int total = num1 + num2 + num3;
@@ -30,6 +30,37 @@ namespace Arrays
 			{
 				Console.WriteLine(num);
 			}
+			// Another way to create a array
+			*/
+
+			int[] newArray = new int[]
+			{
+				11,12,13,14,15,16,17,18,19,1,2,3,4,5,6,7,8,9,10
+			};
+
+            // in order to sort an array we can use the following
+
+            string test = " ";
+			test = test.Replace(" ", ""); // Returns the string replacing " " with ""
+
+            Array.Sort(newArray); // Void return so you do not need newArray 
+
+            /*foreach (int num in newArray)
+			{
+                Console.Write($"{num} ");
+            }*/
+
+			Array.Reverse(newArray);
+
+			Array.Clear(newArray, 3, 7);
+
+            foreach (int num in newArray)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine("");
+            
+
         }
 	}
 }
